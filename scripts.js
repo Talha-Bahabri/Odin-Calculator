df8f135d3da09f7c61c439fd4aa764ec40d21d57
Talha <Talha_Future@hotmail.com>
2021-11-13 21:32:32 +0300

    added all the buttons names without any functions

--- scripts.js
+++ scripts.js
@@ -1,63 +1,46 @@
 
-console.log(`happy me start over `);
 
-const operSel = document.querySelector(`.operation`);
-
-const btnn = document.querySelector(`.btnDEL`);
-
-btnn.addEventListener("click" ,  () => {
-    console.log(`before`);
-    operSel.textContent(`gggggggg`)
-    console.log(`after`);
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     const operationSelector = document.querySelector(".operation")
-//     const userInputSelector = document.querySelector(`.userInput`);
+    const operationSelector = document.querySelector(".operation")
+    const userInputSelector = document.querySelector(`.userInput`);
     
-//     const btnCLR = document.querySelector(`.btnCLR`);
+    const btnCLR = document.querySelector(`.btnCLR`);
 
-//     btnCLR.addEventListener("click" , () => {
-//         operationSelector.textContent(`gg`);
-        
+    btnCLR.addEventListener("click" , () => {
+        operationSelector.textContent = ``;
+        userInputSelector.textContent = ``;
+      });
 
-//       });
+    const btnDEL = document.querySelector(`.btnDEL`);
+    btnDEL.addEventListener("click" , () => {
+    userInputSelector.textContent = `DELETE ONE NUMBER FORM USERINPUT`;
 
-// const btnDEL = document.querySelector(`.btnDEL`);
+        });
 
-// btnDEL.addEventListener("click" , () => {
-//     console.log(`before`);
-//     userInputSelector.textContent(`gg`);
-//     console.log(`after`);
+    
+        const btn1 = document.querySelector(`.btn1`);
+        const btn2 = document.querySelector(`.btn2`);
+        const btn3 = document.querySelector(`.btn3`);
+        const btnMUL = document.querySelector(`.btnMUL`);
 
-//   });
+        const btn4 = document.querySelector(`.btn4`);
+        const btn5 = document.querySelector(`.btn5`);
+        const btn6 = document.querySelector(`.btn6`);
+        const btnDIV = document.querySelector(`.btnDIV`);
+
+        const btn7 = document.querySelector(`.btn7`);
+        const btn8 = document.querySelector(`.btn8`);
+        const btn9 = document.querySelector(`.btn9`);
+        const btnADD = document.querySelector(`.btnADD`);
+
+        const btn0 = document.querySelector(`.btn0`);
+        const btnDOT = document.querySelector(`.btnDOT`);
+        const btnSUB = document.querySelector(`.btnSUB`);
+        const btnEQL = document.querySelector(`.btnEQL`);
 
 
 
 //From this point i'll add all the  btns
 
 
-
-
-// function bbb() {
-    
-//     let btt = document.querySelector(".btnCLR");
-    
-// }
 
 //here the btns funtions finishes.
