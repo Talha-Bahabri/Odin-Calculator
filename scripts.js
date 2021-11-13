328affc7c52ab4c607be40a8f7f6018655e70cbc
Talha <Talha_Future@hotmail.com>
2021-11-13 21:19:33 +0300

    nothing .. i'm trying to fix an issue,  deleted the code and retrying

--- scripts.js
+++ scripts.js
@@ -1,18 +1,63 @@
-const operationSelector = document.querySelector(`.operation`);
-const userInputSelector = document.querySelector(`.userInput`);
 
+console.log(`happy me start over `);
+
+const operSel = document.querySelector(`.operation`);
+
+const btnn = document.querySelector(`.btnDEL`);
+
+btnn.addEventListener("click" ,  () => {
+    console.log(`before`);
+    operSel.textContent(`gggggggg`)
+    console.log(`after`);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     const operationSelector = document.querySelector(".operation")
+//     const userInputSelector = document.querySelector(`.userInput`);
     
+//     const btnCLR = document.querySelector(`.btnCLR`);
+
+//     btnCLR.addEventListener("click" , () => {
+//         operationSelector.textContent(`gg`);
+        
+
+//       });
+
+// const btnDEL = document.querySelector(`.btnDEL`);
+
+// btnDEL.addEventListener("click" , () => {
+//     console.log(`before`);
+//     userInputSelector.textContent(`gg`);
+//     console.log(`after`);
+
+//   });
+
+
 
 //From this point i'll add all the  btns
 
-const btnCLR = document.querySelector(`.btnCLR`);
+
+
+
+// function bbb() {
     
-    btnCLR.addEventListener("click" , () => {
-        operationSelector.textContent(` d`);
-        userInputSelector.textContent(` d`);
-
-      });
-
-
+//     let btt = document.querySelector(".btnCLR");
+    
+// }
 
 //here the btns funtions finishes.
