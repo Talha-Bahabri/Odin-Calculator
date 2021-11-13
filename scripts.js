694754e86dc93e0ce6466c0c79dce7db529db20d
Talha <Talha_Future@hotmail.com>
2021-11-13 21:36:29 +0300

    added writing number when numbers are clicked

--- scripts.js
+++ scripts.js
@@ -18,21 +18,66 @@
 
     
         const btn1 = document.querySelector(`.btn1`);
+        btn1.addEventListener("click" , () => {
+            userInputSelector.textContent = `1`;
+        
+                });
+                
         const btn2 = document.querySelector(`.btn2`);
+        btn2.addEventListener("click" , () => {
+            userInputSelector.textContent = `2`;
+        
+                });
         const btn3 = document.querySelector(`.btn3`);
-        const btnMUL = document.querySelector(`.btnMUL`);
+        btn3.addEventListener("click" , () => {
+            userInputSelector.textContent = `3`;
+        
+                });
+
+                
+
 
         const btn4 = document.querySelector(`.btn4`);
+        btn4.addEventListener("click" , () => {
+            userInputSelector.textContent = `4`;
+        
+                });
         const btn5 = document.querySelector(`.btn5`);
+        btn5.addEventListener("click" , () => {
+            userInputSelector.textContent = `5`;
+        
+                });
         const btn6 = document.querySelector(`.btn6`);
-        const btnDIV = document.querySelector(`.btnDIV`);
+        btn6.addEventListener("click" , () => {
+            userInputSelector.textContent = `6`;
+        
+                });
 
         const btn7 = document.querySelector(`.btn7`);
+        btn7.addEventListener("click" , () => {
+            userInputSelector.textContent = `7`;
+        
+                });
         const btn8 = document.querySelector(`.btn8`);
+        btn8.addEventListener("click" , () => {
+            userInputSelector.textContent = `8`;
+        
+                });
         const btn9 = document.querySelector(`.btn9`);
-        const btnADD = document.querySelector(`.btnADD`);
+        btn9.addEventListener("click" , () => {
+            userInputSelector.textContent = `9`;
+        
+                });
 
         const btn0 = document.querySelector(`.btn0`);
+        btn0.addEventListener("click" , () => {
+            userInputSelector.textContent = `0`;
+        
+                });
+
+        const btnMUL = document.querySelector(`.btnMUL`);
+        const btnDIV = document.querySelector(`.btnDIV`);
+        const btnADD = document.querySelector(`.btnADD`);
         const btnDOT = document.querySelector(`.btnDOT`);
         const btnSUB = document.querySelector(`.btnSUB`);
         const btnEQL = document.querySelector(`.btnEQL`);
