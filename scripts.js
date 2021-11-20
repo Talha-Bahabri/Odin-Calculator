066165a3726e80ae0907b317f780070a1f8effe2
Talha <Talha_Future@hotmail.com>
2021-11-20 21:48:58 +0300

    added two functions for onclicknumber and onclickoperand for clearer code

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@
     const userInputSelector = document.querySelector(`.userInput`);
 
     let tempValuesInput = `` ;
-    // let inputsArray = [];
     let num1 ;
     let num2 ;
     let operand = ``;
@@ -13,132 +12,96 @@
     let isEqlPressed = false;
     
 //From this point i'll add all the  btns
+// window.addEventListener(`keydown`, (event) => { 
+//     if (event.key == `Backspace`) {
+//         alert(`bababoya`);
+//     }
+// });
 
     const btnCLR = document.querySelector(`.btnCLR`);
     btnCLR.addEventListener("click" , clr );
 
     const btnDEL = document.querySelector(`.btnDEL`);
     btnDEL.addEventListener("click" , del );
+    window.addEventListener("keydown", (event) => {
+        if (event.key == `Backspace` || event.key == `Delete` ) {
+          del();
+        }
+      });
 
     
+    
         const btn1 = document.querySelector(`.btn1`);
-        btn1.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`1`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn1.addEventListener("click" , function(e) {
+            onNumberClick(1);
+        });
+
+
         const btn2 = document.querySelector(`.btn2`);
-        btn2.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`2`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn2.addEventListener("click" , function(e) {
+            onNumberClick(2);
+        });
 
         const btn3 = document.querySelector(`.btn3`);
-        btn3.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`3`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn3.addEventListener("click" , function(e) {
+            onNumberClick(3);
+        });
         const btn4 = document.querySelector(`.btn4`);
-        btn4.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`4`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn4.addEventListener("click" , function(e) {
+            onNumberClick(4);
+        });
         const btn5 = document.querySelector(`.btn5`);
-        btn5.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`5`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn5.addEventListener("click" ,function(e) {
+            onNumberClick(5);
+        });
         const btn6 = document.querySelector(`.btn6`);
-        btn6.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`6`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn6.addEventListener("click" ,function(e) {
+            onNumberClick(6);
+        });
 
         const btn7 = document.querySelector(`.btn7`);
-        btn7.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`7`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn7.addEventListener("click" ,function(e) {
+            onNumberClick(7);
+        });
         const btn8 = document.querySelector(`.btn8`);
-        btn8.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`8`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn8.addEventListener("click" ,function(e) {
+            onNumberClick(8);
+        });
         const btn9 = document.querySelector(`.btn9`);
-        btn9.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`9`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn9.addEventListener("click" ,function(e) {
+            onNumberClick(9);
+        });
         const btn0 = document.querySelector(`.btn0`);
-        btn0.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`0`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btn0.addEventListener("click" ,function(e) {
+            onNumberClick(0);
+        });
         const btnDOT = document.querySelector(`.btnDOT`);
-        btnDOT.addEventListener("click" , () => {
-            tempValuesInput = tempValuesInput.concat(`.`);
-            userInputSelector.textContent = tempValuesInput;
-        
-                });
+        btnDOT.addEventListener("click" ,function(e) {
+            onNumberClick(`.`);
+        });
 
 
 
         const btnADD = document.querySelector(`.btnADD`);
-        btnADD.addEventListener("click" , () => {
-                // this firstTime is only for showing the sign when entering for first time
-            if(firstTime){
-                operand = `+`;
-                }
-
-            conditionsForOperations();
-
-            operand = `+`;
-
-                });          
+        btnADD.addEventListener("click" , function(e) {
+            onOperandClick(`+`);
+        });          
 
 
         const btnMUL = document.querySelector(`.btnMUL`);
-        btnMUL.addEventListener("click" , () => {
-            if(firstTime){
-            operand = `*`;
-            }
-    
-            conditionsForOperations();
-            operand = `*`;
+        btnMUL.addEventListener("click" , function(e) {
+            onOperandClick(`*`);
+        }); 
 
-                });
         const btnDIV = document.querySelector(`.btnDIV`);
-        btnDIV.addEventListener("click" , () => {
-            if(firstTime){
-            operand = `/`;
-            }
-    
-            conditionsForOperations();
-            operand = `/`;
+        btnDIV.addEventListener("click" , function(e) {
+            onOperandClick(`/`);
+        });       
 
-        
-                });
-        
-        
         const btnSUB = document.querySelector(`.btnSUB`);
-        btnSUB.addEventListener("click" , () => {
-            if(firstTime){
-            operand = `-`;
-            }
-    
-            conditionsForOperations();
-            operand = `-`;
-
-                });
+        btnSUB.addEventListener("click" , function(e) {
+            onOperandClick(`-`);
+        });       
 
         const btnEQL = document.querySelector(`.btnEQL`);
         btnEQL.addEventListener("click" , () => {
@@ -150,8 +113,23 @@
 
                 });
 
+ 
 
+        function onNumberClick(theNumber) {
 
+            tempValuesInput = tempValuesInput.concat(theNumber);
+            userInputSelector.textContent = tempValuesInput;
+        }
+
+        function onOperandClick(theOperand) {
+            if(firstTime){
+            operand = `${theOperand}`;
+            }
+    
+            conditionsForOperations();
+            operand = `${theOperand}`;
+
+        }
 
         function conditionsForOperations() {
 
@@ -163,7 +141,6 @@
 
             //if the last elemnt in the is number, or not empty , it will enter and add the numbers , at the end 
                                                     // it will add the operand 
-            // typeof inputsArray.at(-1) == `number` || 
             if(tempValuesInput != '') {
                 if(firstTime) {
                     num1 = parseFloat(tempValuesInput);
@@ -275,22 +252,3 @@
             isEqlPressed = false;
 
         }
-
-
-
-
-//here the btns funtions finishes.
-
-// a variable will hold the numbers as string and every num btn clicked will add to the string then change it to float
-// will create an array , this array will hold the entered values IF AND ONLY IF one of the operations btns are clicked
-//      * The array would be like : array = ["1.5" , " + " , "10" , "-" , "20"] 
-//      * MAKE SURE THE USER CAN'T ENTER TWO OPERATIONS TOGETHER , to do that check if the last elemnt is num or OP
-//      * result = 0  num = 0  op = ""  = 
-//      * enter loop  for each elemnt , check if the elemnt is a number. num = elemnt CONTINUE to restart the loop
-//      * if you want you can check online how to check if the datatype are eql
-//      * the second elemnt will be OP , result = result OP num ... repeat = 
-//                      === for example  result = 1.5 + 10  = 
-//      *
-// once the EQL operation is clicked the userInput will be removed and the result will be displayed there = 
-
-
